40513a77f9bc47a8afcf87e588fce7b7ecf6c787
Ágata Santos <htasantos1@outlook.com>
2026-02-07 23:46:44 -0300

    Delete ALGORITHMIC THINKING directory

--- ALGORITHMIC THINKING/orcamento_imobiliario/contrato.py
+++
@@ -1,8 +0,0 @@
-class Contrato:
-    def __init__(self):
-        self.valor = 2000
-
-    def parcelar(self, parcelas):
-        if parcelas > 5:
-            parcelas = 5
-        return self.valor / parcelas
